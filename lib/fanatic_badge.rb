bf142d642a683d5ce03329d6d006cc476d3704ea
Zhdanov <evdev34@gmail.com>
2021-08-06 12:06:40 +0300

    update css classes, reorganize

--- lib/fanatic_badge.rb
+++ lib/fanatic_badge.rb
@@ -2,21 +2,43 @@ require 'capybara/session'
 
 module FanaticBadge
   STACKOVERFLOW_URL = 'https://stackoverflow.com/'.freeze
+  REVIEW_URL = "#{STACKOVERFLOW_URL}review/".freeze
+  LOGIN_URL = "#{STACKOVERFLOW_URL}users/login/".freeze
 
-  def self.run
-    session = Capybara::Session.new(:selenium_chrome_headless)
+  class CookiesButtonNotFound < StandardError; end
 
-    session.visit STACKOVERFLOW_URL + 'users/login/'
-    session.find('.grid--cell.s-btn.s-btn__primary.js-accept-cookies.js-consent-banner-hide').click
-    session.within '#login-form' do
-      session.fill_in 'email', with: CREDENTIALS['stackoverflow']['email']
-      session.fill_in 'password', with: CREDENTIALS['stackoverflow']['password']
+  class FormInputNotFound < StandardError; end
+
+  class << self
+    def run
+      session = Capybara::Session.new(:selenium_chrome_headless)
+
+      session.visit LOGIN_URL
+      accept_cookies(session)
+      fill_form(session)
+
+      session.find('#submit-button').click
+
+      sleep 3
+      session.visit REVIEW_URL
+      sleep 3
     end
 
-    session.find('#submit-button').click
+    private
 
-    sleep 3
-    session.visit STACKOVERFLOW_URL + 'review/'
-    sleep 3
+    def accept_cookies(session)
+      session.find('.js-accept-cookies').click
+    rescue Capybara::ElementNotFound
+      raise CookiesButtonNotFound
+    end
+
+    def fill_form(session)
+      session.within '#login-form' do
+        session.fill_in 'email', with: CREDENTIALS['stackoverflow']['email']
+        session.fill_in 'password', with: CREDENTIALS['stackoverflow']['password']
+      end
+    rescue Capybara::ElementNotFound
+      raise FormInputNotFound
+    end
   end
 end
